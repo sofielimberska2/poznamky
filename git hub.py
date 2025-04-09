9effbcf1781dfce974c366f828227e39a98eaad4
unknown <sofie.limberska@infis.cz>
2025-04-09 13:10:55 +0200

    ahoj

--- git hub.py
+++ git hub.py
@@ -1,9 +1,36 @@
 import random 
+cislo1= int(input ("zadej mi cislo"))
+cislo8= int(input("zadej cislo8"))
+
+vysledek = []
+
+if znamenko == "-":
+   secret = cislo8 -cislo1
+   vysledek.append(soucet)
+elif znamenko == "+":
+     soucin = cislo1 * cislo8 
+     vysledek.append(soucin)
+elif znamenko = "/": 
+     podil = cislo1 / cislo8
+     vysledek.append(podil)
+else:
+    print("spatnen znamenko")
+print (random.choice{vzsledky})
+print (vysledky)
 
 
-barvy = ["cervena" , "modra" , "zelena" ] 
-print (barvy[1])
-
-random.choice (barvy)
 
 
+import random 
+polozky = ["kabat", "chleba", "syr", "automobil", "bicikl"]
+for _ in range (10):
+    osoba = random.choice (polozky)
+    osoba2 = random.choice (polozky)
+    
+    print(f"- kupme jiny {osoba1}")
+    
+    if osoba1 == osoba2:
+         print(f" - ne, kupme jiny {osoba1}")
+    else: 
+         print(f"- ne kupme {osoba2}")
+    
